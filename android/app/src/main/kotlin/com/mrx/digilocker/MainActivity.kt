21de7aff3850780a5970429340330a8daa9b54f0
mrxdevs <flutter.amjad@gmail.com>
2025-09-29 13:23:08 +0530

    feat: Update package namespace and integrate app_links for deep linking support

--- android/app/src/main/kotlin/com/mrx/digilocker/MainActivity.kt
+++ android/app/src/main/kotlin/com/mrx/digilocker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.digilocker
+package com.mrx.digilocker
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:app_links/app_links.dart';
 import 'package:digilocker/presentations/homepage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  final appLinks = AppLinks(); // AppLinks is singleton
+
+  // Subscribe to all events (initial link and further)
+  final sub = appLinks.uriLinkStream.listen((uri) {
+    // Do something (navigation, ...)
+  });
   runApp(const MyApp());
 }
 
